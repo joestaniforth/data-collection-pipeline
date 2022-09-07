a1415f179ad895cf4c8410bea0e53fd18d466bc1
jsbioinfoportfolio <joseph.staniforth@gmail.com>
2022-09-07 18:14:11 +0100

    test init and methods called in init in one function

--- test.py
+++ test.py
@@ -5,11 +5,9 @@ import unittest
 class TestScraper(unittest.TestCase):
     
     @patch(dotaScraper.driver.find_element)
-    def test_cookies_case(self, mock_driver):
+    def test_init(self, mock_driver, mock_cookies, mock_heroes):
         dS = dotaScraper(url = 'https://www.dotabuff.com/')
-        dS.connect_cookies()
         self.assertTrue(mock_driver.called)
+        self.assertTrue(mock_cookies.called)
+        self.assertTrue(mock_heroes.called)
 
-    def test_get_heroes(self, mock_driver):
-        dS = dotaScraper(url = 'https://www.dotabuff.com/')
-        ds
