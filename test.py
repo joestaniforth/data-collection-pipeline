443a9ad4e58a4af48013e58aded40855e8d2dd33
jsbioinfoportfolio <joseph.staniforth@gmail.com>
2022-09-07 21:22:55 +0100

    added second test

--- test.py
+++ test.py
@@ -1,13 +1,23 @@
-from scraper import dotaScraper
+import os.path
+import scraper
 from unittest.mock import patch
 import unittest
 
 class TestScraper(unittest.TestCase):
     
-    @patch(dotaScraper.driver.find_element)
-    def test_init(self, mock_driver, mock_cookies, mock_heroes):
-        dS = dotaScraper(url = 'https://www.dotabuff.com/')
-        self.assertTrue(mock_driver.called)
+    @patch('scraper.dotaScraper.connect_cookies')
+    @patch('scraper.dotaScraper.get_heroes')
+    def test_init(self,  mock_cookies, mock_heroes):
+        dS = scraper.dotaScraper(url = 'https://www.dotabuff.com/')
         self.assertTrue(mock_cookies.called)
         self.assertTrue(mock_heroes.called)
+        self.assertTrue(os.path.isdir('raw_data'))
 
+
+    def test_get_heroes(self):
+        dS = scraper.dotaScraper(url = 'https://www.dotabuff.com/')
+        self.assertEqual(len(dS.hero_urls), 123)
+        self.assertEqual
+
+if __name__ == '__main__':
+    unittest.main()
