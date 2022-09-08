a997d0b23bca45b9bff79a71d1017705e9e69b17
Joe Staniforth <joseph.staniforth@gmail.com>
2022-09-08 09:23:59 +0100

    added test_scrape_hero

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import os.path
 import scraper
 from unittest.mock import patch
 import unittest
+import json
 
 class TestScraper(unittest.TestCase):
     
@@ -13,11 +14,18 @@ class TestScraper(unittest.TestCase):
         self.assertTrue(mock_heroes.called)
         self.assertTrue(os.path.isdir('raw_data'))
 
-
     def test_get_heroes(self):
         dS = scraper.dotaScraper(url = 'https://www.dotabuff.com/')
         self.assertEqual(len(dS.hero_urls), 123)
-        self.assertEqual
+
+    def test_scrape_heroes(self):
+        dS = scraper.dotaScraper(url = 'https://www.dotabuff.com/')
+        dS.scrape_hero_data(dS.hero_urls[0])
+        self.assertTrue(os.path.isfile('raw_data\\abaddon\\data.json'))
+        with open('raw_data\\abaddon\\data.json', 'r') as file:
+            values = json.load(file)
+        for key in values:
+            self.assertNotEqual(values[key], '')
 
 if __name__ == '__main__':
     unittest.main()
